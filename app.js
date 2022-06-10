31e84dc8924db40c53d85cf350b8e6676fa6fcf2
aksuanil <aksuanil25@gmail.com>
2022-06-10 15:26:16 +0300

    data models && ejs

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const translate = require('translate-google')
+import fetch from 'node-fetch';
+import express from 'express';
+import translate from 'translate-google';
+import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-var bodyParser = require('body-parser')
 var jsonParser = bodyParser.json()
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
@@ -26,32 +27,101 @@ app.post('/', urlencodedParser, async (req, res) => {
     res.render('pages/index', {
         foods: foodArray
     })
+    console.log(foodArray[0]);
+})
+
+app.post('/add', urlencodedParser, async (req, res) => {
+    foodArray = [];
+    const search = await req.body.postFood;
+    const obj = JSON.parse(search);
+    //Mongo Insert
+    await insertFoodData(obj);
 })
 
 class Food {
-    constructor(foodName, foodNameTurkish, proteinNutrientName, proteinValue, proteinUnitName, fatNutrientName, fatValue, fatUnitName, carbohydrateNutrientName, carbohydrateValue, carbohydrateUnitName, energyNutrientName, energyValue, energyUnitName, measureArr) {
-        this.foodInfo = {
-            foodName : foodName,
-            foodNameTurkish : foodNameTurkish,
-        };
+    constructor(foodName, foodNameTurkish, foodCategory,
+        protein,
+        fat,
+        carbohydrate,
+        energy,
+        vitaminA,
+        vitaminE,
+        vitaminD,
+        vitaminC,
+        vitaminB6,
+        vitaminB12,
+        vitaminK,
+        vitaminB1,
+        vitaminB2,
+        vitaminB3,
+        calcium,
+        iron,
+        magnesium,
+        phosphorus,
+        potassium,
+        sodium,
+        zinc,
+        copper,
+        selenium,
+        water,
+        sugar,
+        caffeine,
+        alcohol,
+        caroteneAlpha,
+        caroteneBeta,
+        folicAcid,
+        cholesterol,
+        choline,
+        lycopene,
+        measureArr) {
+        this.foodName = foodName,
+            this.foodNameTurkish = foodNameTurkish,
+            this.foodCategory = foodCategory,
 
-        this.foodNutrients = {
-            proteinNutrientName : proteinNutrientName,
-            proteinValue : proteinValue,
-            proteinUnitName : proteinUnitName,
-            fatNutrientName : fatNutrientName,
-            fatValue : fatValue,
-            fatUnitName : fatUnitName,
-            carbohydrateNutrientName : carbohydrateNutrientName,
-            carbohydrateValue : carbohydrateValue,
-            carbohydrateUnitName : carbohydrateUnitName,
-            energyNutrientName : energyNutrientName,
-            energyValue : energyValue,
-            energyUnitName : energyUnitName,
+            this.nutrients = {
+                protein: protein,
+                carbohydrate: carbohydrate,
+                fat: fat,
+                energy: energy,
+            };
+        this.vitamins = {
+            vitaminA: vitaminA,
+            vitaminB1: vitaminB1,
+            vitaminB2: vitaminB2,
+            vitaminB3: vitaminB3,
+            vitaminB6: vitaminB6,
+            vitaminB12: vitaminB12,
+            vitaminC: vitaminC,
+            vitaminD: vitaminD,
+            vitaminE: vitaminE,
+            vitaminK: vitaminK,
+        };
+        this.minerals = {
+            calcium: calcium,
+            iron: iron,
+            magnesium: magnesium,
+            phosphorus: phosphorus,
+            potassium: potassium,
+            sodium: sodium,
+            zinc: zinc,
+            copper: copper,
+            selenium: selenium,
+        };
+        this.misc = {
+            water: water,
+            sugar: sugar,
+            caffeine: caffeine,
+            alcohol: alcohol,
+            caroteneAlpha: caroteneAlpha,
+            caroteneBeta: caroteneBeta,
+            folicAcid: folicAcid,
+            cholesterol: cholesterol,
+            choline: choline,
+            lycopene: lycopene,
         };
 
         this.foodMeasures = measureArr;
-        
+
         foodArray.push(this);
     }
 };
@@ -66,56 +136,153 @@ async function getFoodDataByName(search) {
     return Promise.all(result.foods.slice(0, 10).map(async (item) => {
         let foodName = item.description
         let foodNameTurkish = await translate(foodName, { to: 'tr' });
-
-        let proteinNutrientName;
-        let proteinValue;
-        let proteinUnitName;
-        let fatNutrientName;
-        let fatValue;
-        let fatUnitName;
-        let carbohydrateNutrientName;
-        let carbohydrateValue;
-        let carbohydrateUnitName;
-        let energyNutrientName;
-        let energyValue;
-        let energyUnitName;
-        let disseminationText;
+        let foodCategory = item.foodCategory;
+        let protein;
+        let fat;
+        let carbohydrate;
+        let energy;
+        let vitaminA;
+        let vitaminE;
+        let vitaminD;
+        let vitaminC;
+        let vitaminB6;
+        let vitaminB12;
+        let vitaminK;
+        let vitaminB1;
+        let vitaminB2;
+        let vitaminB3;
+        let calcium;
+        let iron;
+        let magnesium;
+        let phosphorus;
+        let potassium;
+        let sodium;
+        let zinc;
+        let copper;
+        let selenium;
+        let water;
+        let sugar;
+        let caffeine;
+        let alcohol;
+        let caroteneAlpha;
+        let caroteneBeta;
+        let folicAcid;
+        let cholesterol;
+        let choline;
+        let lycopene;
+        let measure;
         let gramWeight;
 
-        item.foodNutrients.slice(0, 10).map((item) => {
+        item.foodNutrients.map((item) => {
             if (item.nutrientId == 1003) // protein
-            {
-                proteinNutrientName = item.nutrientName;
-                proteinValue = item.value;
-                proteinUnitName = item.unitName;
-            }
+                protein = { "value": item.value, "unitName": item.unitName }
             else if (item.nutrientId == 1004) // fat 
-            {
-                fatNutrientName = item.nutrientName;
-                fatValue = item.value;
-                fatUnitName = item.unitName;
-            }
+                fat = { "value": item.value, "unitName": item.unitName }
             else if (item.nutrientId == 1005) // Carbohydrate
-            {
-                carbohydrateNutrientName = item.nutrientName;
-                carbohydrateValue = item.value;
-                carbohydrateUnitName = item.unitName;
-            }
+                carbohydrate = { "value": item.value, "unitName": item.unitName }
             else if (item.nutrientId == 1008) // Energy
-            {
-                energyNutrientName = item.nutrientName;
-                energyValue = item.value;
-                energyUnitName = item.unitName;
-            }
+                energy = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 1106) // A
+                vitaminA = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 1109) // E 
+                vitaminE = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 1114) // D
+                vitaminD = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 1162) // C
+                vitaminC = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 1175) // B6
+                vitaminB6 = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 1178) // B12
+                vitaminB12 = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 1185) // K
+                vitaminK = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 1165) // B1
+                vitaminB1 = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 1166) // B2
+                vitaminB2 = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 1167) // B3
+                vitaminB3 = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 1087) // Calcium
+                calcium = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 1089) // Iron
+                iron = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 1090) // magnesium
+                magnesium = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 1091) // phosphorus
+                phosphorus = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 1092) // potassium
+                potassium = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 1093) // sodium
+                sodium = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 1095) // zinc
+                zinc = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 1098) // copper
+                copper = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 1103) // selenium
+                selenium = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 1051) // water
+                water = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 2000) // sugar
+                sugar = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 1057) // caffeine
+                caffeine = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 1018) // alcohol
+                alcohol = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 1108) // caroteneAlpha
+                caroteneAlpha = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 1107) // caroteneBeta
+                caroteneBeta = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 1186) // folicAcid
+                folicAcid = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 1253) // cholesterol
+                cholesterol = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 1180) // choline
+                choline = { "value": item.value, "unitName": item.unitName }
+            else if (item.nutrientId == 1122) // lycopene
+                lycopene = { "value": item.value, "unitName": item.unitName }
         })
         let measureArr = [];
-        item.foodMeasures.slice(0, 5).map((measure) => {
-            disseminationText = measure.disseminationText;
-            gramWeight = measure.gramWeight;
-            measureArr.push( {disseminationText : disseminationText, gramWeight : gramWeight });
+        item.foodMeasures.slice(0, 5).map((item) => {
+            measure = item.disseminationText;
+            gramWeight = item.gramWeight;
+            measureArr.push({ measure: measure, gramWeight: gramWeight });
         });
 
-        new Food(foodName, foodNameTurkish, proteinNutrientName, proteinValue, proteinUnitName, fatNutrientName, fatValue, fatUnitName, carbohydrateNutrientName, carbohydrateValue, carbohydrateUnitName, energyNutrientName, energyValue, energyUnitName, measureArr);
+        new Food(foodName, foodNameTurkish, foodCategory,
+            protein,
+            fat,
+            carbohydrate,
+            energy,
+            vitaminA,
+            vitaminE,
+            vitaminD,
+            vitaminC,
+            vitaminB6,
+            vitaminB12,
+            vitaminK,
+            vitaminB1,
+            vitaminB2,
+            vitaminB3,
+            calcium,
+            iron,
+            magnesium,
+            phosphorus,
+            potassium,
+            sodium,
+            zinc,
+            copper,
+            selenium,
+            water,
+            sugar,
+            caffeine,
+            alcohol,
+            caroteneAlpha,
+            caroteneBeta,
+            folicAcid,
+            cholesterol,
+            choline,
+            lycopene,
+            measureArr);
     }))
 
 }
